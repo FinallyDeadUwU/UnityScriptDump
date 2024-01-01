f3d870bfd32445f44297699e1a971daf7d2b5703
0x0001 <86870860+FinallyDeadUwU@users.noreply.github.com>
2024-01-02 00:10:35 +0100

    Update FlipflopBool.cs

--- FlipflopBool.cs
+++ FlipflopBool.cs
@@ -4,7 +4,7 @@ public static class FlipflopBool
     
     public static bool FlipFlop(bool Value)
     {
-        Value = !Value;
+        Value = !Value; //You can just grab this tbh. But it looks cool right?
         return Value;
     }
 }
